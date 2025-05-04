be90f927dd489560c612cabe5c90931e2233d07a
michunie99 <mniedbala99@gmail.com>
2025-05-04 14:30:16 +0200

    feat: initial settings file

---
+++ src/settings.h
@@ -0,0 +1,12 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+const int PLAYER_SPEED = 500;
+const int LASER_SPEED = 600;
+const int METEOR_SPEED_RANGE[2] = {300, 400};
+const float METEOR_TIMER_DURATION = 0.4;
+
+#endif
